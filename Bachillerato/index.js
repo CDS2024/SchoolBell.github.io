1a72e66c1033942c41641356b6abbee374ffc7f1
CDS2024 <157392408+CDS2024@users.noreply.github.com>
2024-01-23 14:55:53 -0500

    Prueba

--- Bachillerato/index.js
+++ Bachillerato/index.js
@@ -1,7 +1,7 @@
 const $tiempo=document.querySelector(".tiempo");
 const $fecha=document.querySelector(".fecha");
 
-let horarioBachillerato= ['6:45:06 a.&nbsp;m.','7:40:00 a.&nbsp;m.','8:30:06 a.&nbsp;m.','9:15:06 a.&nbsp;m.','9:45:00 a.&nbsp;m.','10:35:06 a.&nbsp;m.','12:15:06 p.&nbsp;m.','1:00:06 p.&nbsp;m.','1:40:06 p.&nbsp;m.','2:30:26 p.&nbsp;m.','2:40:00 p.&nbsp;m.','2:52:00 p.&nbsp;m.']
+let horarioBachillerato= ['6:45:06 a.&nbsp;m.','7:40:00 a.&nbsp;m.','8:30:06 a.&nbsp;m.','9:15:06 a.&nbsp;m.','9:45:00 a.&nbsp;m.','10:35:06 a.&nbsp;m.','12:15:06 p.&nbsp;m.','1:00:06 p.&nbsp;m.','1:40:06 p.&nbsp;m.','2:30:26 p.&nbsp;m.','2:40:00 p.&nbsp;m.','2:52:00 p.&nbsp;m.', "14:59:00", "13:00:00",'2:59:00 p.&nbsp;m.']
 
 function digitalClock() {
     let f= new Date(),
@@ -9,9 +9,7 @@ function digitalClock() {
     mes=  f.getMonth(),
     anio= f.getFullYear(),
     diaSemana = f.getDay();
-    let timeString = f.toLocaleTimeString({
-        hour12: true,
-        timeZone:'America/Bogota'});
+    let timeString = f.toLocaleTimeString();
     $tiempo.innerHTML = timeString;
     $fecha.innerHTML = `${anio}-${mes+1}-${dia}`
     console.log($tiempo.innerHTML);
