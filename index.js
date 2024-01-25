95684dad3114662f96e815ecfe0a4923a945f168
CDS2024 <157392408+CDS2024@users.noreply.github.com>
2024-01-25 13:55:28 -0500

    prueba de horarios 2

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function onDisplay() {
 
 bachillerato.addEventListener("click", ()=> {
     onDisplay(); 
-    let horarioBachillerato= ['6:45:06 AM','7:40:00 AM','8:35:06 AM','9:05:06 AM','10:00:00 AM','10:55:06 AM','11:50:06 AM','12:50:06 PM','1:40:06 PM','1:54:06 PM','1:55:06 PM','2:30:00 PM'];
+    let horarioBachillerato= ['6:45:06 AM','7:40:00 AM','8:35:06 AM','9:05:06 AM','10:00:00 AM','10:55:06 AM','11:50:06 AM','12:50:06 PM','1:40:06 PM','1:58:06 PM','1:59:06 PM','2:30:00 PM'];
     function digitalClock() {
         let f= new Date(),
         dia = f.getDate(),
